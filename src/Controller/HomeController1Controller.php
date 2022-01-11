36dd7e304b4affd3b41dba42ed34f713c8a2a0c0
axelcrz <95704856+axelcrz@users.noreply.github.com>
2022-01-11 15:08:16 +0100

    Update HomeController1Controller.php

--- src/Controller/HomeController1Controller.php
+++ src/Controller/HomeController1Controller.php
@@ -9,12 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController1Controller extends AbstractController
 {
     /**
-     * @Route("/home/controller1", name="home_controller1")
+     * @Route("/test", name="test")
      */
     public function index(): Response
     {
-        return $this->render('home_controller1/index.html.twig', [
-            'controller_name' => 'HomeController1Controller',
+        return $this->render('test.html.twig', [
+            'index' => 'index',
         ]);
     }
 }
