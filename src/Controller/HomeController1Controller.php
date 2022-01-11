a2d1993202dafaf05b57297efeea2a5cb2dfd7a6
axelcrz <95704856+axelcrz@users.noreply.github.com>
2022-01-11 11:37:04 +0100

    Create HomeController1Controller.php

---
+++ src/Controller/HomeController1Controller.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController1Controller extends AbstractController
+{
+    /**
+     * @Route("/home/controller1", name="home_controller1")
+     */
+    public function index(): Response
+    {
+        return $this->render('home_controller1/index.html.twig', [
+            'controller_name' => 'HomeController1Controller',
+        ]);
+    }
+}
+{
+    /**
+     * @Route("/home/controller1", name="home_controller1")
+     */
+
+    {
+        return $this->render('test.html.twig', [
+            'controller_name' => 'HomeController1Controller',
+        ]);
+    }
+}
